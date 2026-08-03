919d171eaad1dd3d6ce5328034168f057a71155e
BraveOPotato <abdullahalkafajy321@gmail.com>
2026-08-03 03:36:33 -0500

    chore(SEO): removed stale reference to FckSignups

--- src/data/schema.js
+++ src/data/schema.js
@@ -2,7 +2,6 @@ export const websiteSchema = {
   "@context": "https://schema.org",
   "@type": "WebSite",
   name: "NoSignups",
-  alternateName: "FckSignups",
   url: "https://nosignups.net/",
   description:
     "A directory of open-source tools that work in the browser without signups.",
