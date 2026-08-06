fa111e5c94676fd2558a0818cd1ddc5b4bc3ad37
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-06 16:20:22 +0300

    refactor(shared): migrate all shared components to CSS Modules

--- src/components/Shared/Feedback/Toast/Toast.tsx
+++ src/components/Shared/Feedback/Toast/Toast.tsx
@@ -1,3 +1,5 @@
+import s from "./Toast.module.css";
+
 interface ToastProps {
   innerText: string;
   onExit: () => void;
@@ -5,15 +7,13 @@ interface ToastProps {
 
 export function Toast({ innerText, onExit }: ToastProps) {
   return (
-    <>
-      <div className="toast-container">
-        <p>{innerText}</p>
-        <button type="button" onClick={onExit} className="close-toast-button">
-          <svg aria-hidden="true">
-            <use href="/icons-sprite.svg#x-mark" />
-          </svg>
-        </button>
-      </div>
-    </>
+    <div className={s.toastContainer}>
+      <p>{innerText}</p>
+      <button type="button" onClick={onExit} className={s.closeToastButton}>
+        <svg aria-hidden="true">
+          <use href="/icons-sprite.svg#x-mark" />
+        </svg>
+      </button>
+    </div>
   );
 }
